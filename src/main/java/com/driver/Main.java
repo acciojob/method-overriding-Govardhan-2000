7a82a089c5a1baef287f879cd38375273c463bc2
Govardhan <kurubagovardhan32@gmail.com>
2023-07-13 15:40:40 +0530

    add to project

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,30 +1,28 @@
 package com.driver;
 
+class A {
+    public String meth() {
+        return "Invoking method from class A";
+    }
+}
+
+class B extends A {
+    @Override
+    public String meth() {
+        return "Method is overridden in Extended class B";
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        Product p = new Product();
+        B obj = new B();
 
-        int result1 = p.product(2, 3);
+        // Calling method meth from class A
+        String result1 = obj.meth();
         System.out.println("Result 1: " + result1);
 
-        int result2 = p.product(2, 3, 4);
+        // Calling overridden method meth from class B
+        String result2 = obj.meth();
         System.out.println("Result 2: " + result2);
-
-        double result3 = p.product(2.5, 3.5);
-        System.out.println("Result 3: " + result3);
-    }
-
-    public static class Product {
-        public int product(int x, int y) {
-            return x * y;
-        }
-
-        public int product(int x, int y, int z) {
-            return x * y * z;
-        }
-
-        public double product(double x, double y) {
-            return x * y;
-        }
     }
 }
